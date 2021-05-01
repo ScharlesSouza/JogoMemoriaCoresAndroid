5306f717e7252d1c1e4f49e748587de2aff887e0
SCHARLES <souza.scharles@gmail.com>
2021-04-30 23:21:33 -0300

    Criando a interface grafica

--- app/src/main/java/com/example/trabalhobimestral/MenuActivity.java
+++ app/src/main/java/com/example/trabalhobimestral/MenuActivity.java
@@ -5,10 +5,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.ProgressBar;
+import android.widget.RadioButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -16,6 +19,10 @@ public class MenuActivity extends AppCompatActivity {
 
     private TextView textViewNome;
     private Button buttonRanking;
+    private RadioButton rButtonCores, rButtonNumeros;
+    private ProgressBar progressBar;
+    private int i;
+
     private String[] cores = {"verde", "vermelha", "azul"};
     private Boolean[] respostas = {false,false,false};
 
@@ -31,21 +38,92 @@ public class MenuActivity extends AppCompatActivity {
         buttonRanking = findViewById(R.id.buttonRanking);
         textViewNome.setText(nome_usuario);
 
+        rButtonCores = findViewById(R.id.rButtonCores);
+        rButtonNumeros = findViewById(R.id.rButtonNumero);
+        progressBar = findViewById(R.id.progressBar);
+
+        rButtonCores.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                abreDesafioCores();
+            }
+
+        });
+        rButtonNumeros.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                abreDesafioNumeros();
+            }
+
+        });
 
 
     }//onCreate
 
-
     public void clickRanking(View view) {
 
-
-
-            Intent intent = new Intent(this, RankingActivity.class);
-            intent.putExtra("nome_usuario", textViewNome.getText().toString());
-            startActivity(intent);
-
+        executarProgressBar();
 
     }//clicarRanking
+
+    public void abreRanking(){
+        Intent intent = new Intent(this, RankingActivity.class);
+        intent.putExtra("nome_usuario", textViewNome.getText().toString());
+        startActivity(intent);
+    }//abreRanking
+
+    private void abreDesafioNumeros() {
+        Intent intent = new Intent(this, RankingActivity.class);
+        intent.putExtra("nome_usuario", textViewNome.getText().toString());
+        startActivity(intent);
+    }//abreDesafioNumeros
+
+    private void abreDesafioCores() {
+        Intent intent = new Intent(this, RankingActivity.class);
+        intent.putExtra("nome_usuario", textViewNome.getText().toString());
+        startActivity(intent);
+    }//abreDesafioCores
+
+
+    private void executarProgressBar(){
+        i=0;
+        Handler handler = new Handler();
+        progressBar.setVisibility(View.VISIBLE);
+        i=progressBar.getProgress();
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while(i<100){
+                    i+=10;
+                    handler.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            progressBar.setProgress(i);
+                            if (i>=100){
+                                abreRanking();
+                            }//if
+                        }//run
+                    });//rundler.post
+                    try {
+                        Thread.sleep(1000);
+                    }catch (InterruptedException erro){
+
+                    }
+                }
+            }//run
+        }).start();//thread
+
+    }//executaProgressBar
+
+    private void abrirDesafio(){
+        if(rButtonNumeros.isChecked()){
+
+        }else if(rButtonCores.isChecked()){
+
+        }
+    }//abrirDesafio
+
 
     public Boolean ler(String nome){
         SharedPreferences sharedPreferences = getSharedPreferences("dados",MODE_PRIVATE);
--- app/src/main/java/com/example/trabalhobimestral/RankingActivity.java
+++ app/src/main/java/com/example/trabalhobimestral/RankingActivity.java
@@ -2,13 +2,40 @@ package com.example.trabalhobimestral;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class RankingActivity extends AppCompatActivity {
+    private Button buttonVoltar;
+    String nome_usuario;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ranking);
+
+        Intent intent = getIntent();
+        nome_usuario = intent.getStringExtra("nome_usuario");
+
+        buttonVoltar=findViewById(R.id.buttonVoltar);
+    }
+    public void cliqueVoltar(View view){
+        chamaMenuActivity();
+    }//cliqueVoltar
+
+    private void chamaMenuActivity(){
+        Intent intent = new Intent(this, MenuActivity.class);
+        intent.putExtra("nome_usuario", nome_usuario);
+        startActivity(intent);
+    }//chamaMenuActivity
+
+    @Override
+    public void onBackPressed(){ //Botão BACK padrão do android
+
+       chamaMenuActivity();//O efeito ao ser pressionado do botão (no caso abre a activity)
+        finishAffinity(); //Método para matar a activity e não deixa-lá indexada na pilhagem
+        return;
     }
 }

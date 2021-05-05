270c02de365629a0d07737e8084f9fb46ad609a0
SCHARLES <souza.scharles@gmail.com>
2021-05-05 16:43:45 -0300

    Jogo das cores finalizado

--- app/src/main/java/com/example/trabalhobimestral/GradeCoresActivity.java
+++ app/src/main/java/com/example/trabalhobimestral/GradeCoresActivity.java
@@ -1,38 +1,61 @@
 package com.example.trabalhobimestral;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import java.util.ArrayList;
 import java.util.Random;
 
 public class GradeCoresActivity extends AppCompatActivity {
     private Button botaoRosa,botaoVerde,botaoRoxo,botaoMarron,botaoAmarelo,botaoAzul,botaoPreto,botaoVermelho,botaolaranjado;
-    private int i;
+    private TextView contCores;
+    private ArrayList<String>  listaCoresMostrada=null;
+    private int i, clique, repeticoes, tentativa;
     private View view;
-    private String[] cores = {"rosa", "azul", "vermelho", "verde", "preto", "amarelo", "roxo", "marrom", "laranjado"};
-    String cor, corAnterior;
+    private String[] cores = {"preto","amarelo","rosa","vermelho", "verde","marrom", "azul","laranjado","roxo"};
+    String cor, corAnterior, nome_usuario;
     private Random rand = new Random();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_grade_cores);
 
-        botaoAmarelo = findViewById(R.id.botao1);
-        botaoAzul = findViewById(R.id.botao2);
-        botaoPreto = findViewById(R.id.botao3);
-        botaoMarron = findViewById(R.id.botao4);
-        botaolaranjado = findViewById(R.id.botao5);
-        botaoRosa = findViewById(R.id.botao6);
-        botaoRoxo = findViewById(R.id.botao7);
-        botaoVerde = findViewById(R.id.botao8);
-        botaoVermelho = findViewById(R.id.botao9);
+        Intent intent = getIntent();
+        nome_usuario = intent.getStringExtra("nome_usuario");
 
+        if(ler(nome_usuario)){
+        }else{
+            gravarUsuarioSP(nome_usuario);
+        }
 
-        mostrarCores();
+        botaoPreto = findViewById(R.id.botao1Preto);
+        botaoAmarelo = findViewById(R.id.botao2);
+        botaoRosa = findViewById(R.id.botao3);
+        botaoVermelho = findViewById(R.id.botao4);
+        botaoVerde = findViewById(R.id.botao5);
+        botaoMarron = findViewById(R.id.botao6);
+        botaoAzul = findViewById(R.id.botao7);
+        botaolaranjado = findViewById(R.id.botao8);
+        botaoRoxo = findViewById(R.id.botao9);
+
+        listaCoresMostrada = new ArrayList<>();
+
+        contCores = findViewById(R.id.textViewContCores);
+
+        clique=0;
+        repeticoes=tentativa+3;
+        mostrarCores(repeticoes);
+        mostraBotoes();
 
     }
 
@@ -46,32 +69,56 @@ public class GradeCoresActivity extends AppCompatActivity {
         return indice;
     }//getCoresIndice
 
-    private void mostrarCores(){
+    public void cliqueBotao(View botao){
+        if(clique<repeticoes){
+            int tag =  Integer.parseInt((String) botao.getTag());
+            Toast.makeText(getApplicationContext(), "Você clicou em "+cores[tag]+"!", Toast.LENGTH_SHORT).show();
+
+            if(cores[tag] == listaCoresMostrada.get(clique)){
+                clique++;//se acerta a cor incrementa o clique ate finalizar a sequencia de cores correta
+            }else{
+                criarDialogErro(clique);//se errar chama o Dialog e grava a tentativa
+            }
+        }
+        if((clique+1)==repeticoes) {
+            criarDialog(clique);//se acertar tudo chama o Dialog e grava a tentativa
+        }
+    }//cliqueBotao
+
+    private String corBotao(int tag){
+        return cores[tag];
+    }//corBotao
+
+    private void mostrarCores(int repeticao){
         i=0;
         Handler handler = new Handler();
-
 
         new Thread(new Runnable() {
             @Override
             public void run() {
-                while(i<=10) {
+                while(i<=repeticao) {
                     i += 1;
 
                     handler.post(new Runnable() {
                         @Override
                         public void run() {
-                            corAnterior = cor;
-                            cor = getCoresRand();
-
-                            //wile para garantir que as cores nao vao repetir em sequencia
-                            while (corAnterior == cor) {
+                            if(i<repeticao){
+                                corAnterior = cor;
                                 cor = getCoresRand();
+
+                                //wile para garantir que as cores nao vao repetir em sequencia
+                                while (corAnterior == cor) {
+                                    cor = getCoresRand();
+                                }
+                                listaCoresMostrada.add(cor);
+                                contCores.setText(""+i);
+                                escondeBotoes();
+                                chamarBotao(cor);
+                            }else{
+                                mostraBotoes();
+                                contCores.setText("Vamos jogar:\n clique na cor seguindo a sequencia mostrada");
                             }
-                            escondeBotoes();
-                            chamarBotao(cor);
-
                         }//run
-
                     });//rundler.post
 
                     try {
@@ -82,8 +129,40 @@ public class GradeCoresActivity extends AppCompatActivity {
                 }
             }//run
         }).start();//thread
-        mostraBotoes();
     }//mostrarCores
+
+    private void gravarPontuacao(int pontuacao, int tentativa) {
+
+        SharedPreferences sharedPreferences = getSharedPreferences("dados", MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        if (sharedPreferences.contains("nome_usuario")){
+            if (sharedPreferences.getString("nome_usuario","sem nome").equals(nome_usuario) ){
+                editor.putString("pontuacao"+tentativa, String.valueOf(pontuacao));
+                editor.putInt("Qnt_Tentativas", tentativa);
+                Boolean resposta = editor.commit();
+                if (resposta) {
+                    Toast.makeText(this, "tentativa:"+tentativa+", Pontuação: "+pontuacao, Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(this, "Não Salvo", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }else{
+            Toast.makeText(this, "ERRO no arquivo", Toast.LENGTH_SHORT).show();
+        }//if
+
+    }//gravar
+    private void gravarUsuarioSP(String nome) {
+
+        SharedPreferences sharedPreferences = getSharedPreferences("dados", MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString("nome_usuario", nome);
+        Boolean resposta = editor.commit();
+        if (resposta) {
+            Toast.makeText(this, "Usuario: "+nome, Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this, "Não Salvo", Toast.LENGTH_SHORT).show();
+        }
+    }//gravarUsuarioSP
 
     private void chamarBotao(String cor){
         if(cor == "rosa")
@@ -115,7 +194,7 @@ public class GradeCoresActivity extends AppCompatActivity {
             botaolaranjado.setVisibility(View.VISIBLE);
             botaoMarron.setVisibility(View.VISIBLE);
             botaoRoxo.setVisibility(View.VISIBLE);
-    }
+    }//mostraBotoes
     private void escondeBotoes(){
             botaoRosa.setVisibility(View.INVISIBLE);
             botaoPreto.setVisibility(View.INVISIBLE);
@@ -126,6 +205,80 @@ public class GradeCoresActivity extends AppCompatActivity {
             botaolaranjado.setVisibility(View.INVISIBLE);
             botaoMarron.setVisibility(View.INVISIBLE);
             botaoRoxo.setVisibility(View.INVISIBLE);
-    }
+    }//escondeBotoes
+
+    //metodo para mostrar Dialog com erro de clique
+    private void criarDialogErro(int pontuacao){
+        AlertDialog.Builder builder = new AlertDialog.Builder(GradeCoresActivity.this);
+        builder.setTitle("Errou! \n Pontuação: "+pontuacao)
+                .setPositiveButton("Tentar de novo", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        clique=0;
+                        tentativa++;
+                        gravarPontuacao(pontuacao, tentativa );//grava a pontuação desta tentativa
+                    }
+                })
+                .setNegativeButton("Sair", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        chamaMenuActivity();//voltar para o menu
+                    }
+                });
+        builder.show();
+    }//criarDialogErro
+
+    //metodo para mostrar Dialog com 100% de acertos
+    private void criarDialog(int pontuacao){
+        AlertDialog.Builder builder = new AlertDialog.Builder(GradeCoresActivity.this);
+        builder.setTitle("Acertou todos, Parabens! \n Pontuação: "+pontuacao)
+                .setPositiveButton("Tentar de novo", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        clique=0;
+                        tentativa++;
+                        gravarPontuacao(pontuacao, tentativa );//grava a pontuação desta tentativa
+                        chamaGradeCoresActivity();
+                    }
+                })
+                .setNegativeButton("Sair", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        clique=0;
+                        tentativa++;
+                        gravarPontuacao(pontuacao, tentativa );//grava a pontuação desta tentativa
+                        chamaMenuActivity();//voltar para o menu
+                    }
+                });
+        builder.show();
+    }//criarDialog
+
+    //metodo para verificar se o usuario ja existe e pegar a quantidade de vezes que aquele usuariou tentou.
+    public Boolean ler(String nome){
+        SharedPreferences sharedPreferences = getSharedPreferences("dados",MODE_PRIVATE);
+        if (sharedPreferences.contains("nome_usuario") ) {
+            if (sharedPreferences.getString("nome_usuario","sem nome").equals(nome) ){
+                tentativa = sharedPreferences.getInt("Qnt_Tentativas", 0);
+                return true;
+            }else
+                return false;
+        }else{
+            return false;
+        }//if
+    }//ler
+
+    //metodo para chamar a MenuActivity
+    private void chamaMenuActivity(){
+        Intent intent = new Intent(this, MenuActivity.class);
+        intent.putExtra("nome_usuario", nome_usuario);
+        startActivity(intent);
+    }//chamaMenuActivity
+
+    // metodo para chamar a chamaGradeCoresActivity
+    private void chamaGradeCoresActivity(){
+        Intent intent = new Intent(this, GradeCoresActivity.class);
+        intent.putExtra("nome_usuario", nome_usuario);
+        startActivity(intent);
+    }//chamaGradeCoresActivity
 
 }
--- app/src/main/java/com/example/trabalhobimestral/RankingActivity.java
+++ app/src/main/java/com/example/trabalhobimestral/RankingActivity.java
@@ -3,13 +3,22 @@ package com.example.trabalhobimestral;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Map;
 
 public class RankingActivity extends AppCompatActivity {
     private Button buttonVoltar;
     String nome_usuario;
+    private TextView textViewScore;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,8 +28,42 @@ public class RankingActivity extends AppCompatActivity {
         Intent intent = getIntent();
         nome_usuario = intent.getStringExtra("nome_usuario");
 
+        textViewScore = findViewById(R.id.textViewScore);
         buttonVoltar=findViewById(R.id.buttonVoltar);
+
+        lerTudo();
     }
+
+    public void lerTudo(){
+        SharedPreferences sharedPreferences = getSharedPreferences("dados",MODE_PRIVATE);
+        if (sharedPreferences.contains("nome_usuario")){
+            if (sharedPreferences.getString("nome_usuario","sem nome").equals(nome_usuario) ){
+                String texto="";
+                Map<String, ?> prefsMap = sharedPreferences.getAll();
+                ArrayList<String> lista = new ArrayList<String>((Collection<? extends String>) prefsMap.values());
+
+                for (Map.Entry<String, ?> entry: prefsMap.entrySet()) {
+                    Log.v("SharedPreferences", entry.getKey() + ":" +
+                            entry.getValue().toString());
+                    Toast.makeText(this, entry.getKey() + ":" +
+                            entry.getValue().toString(), Toast.LENGTH_SHORT).show();
+                    texto+=entry.getKey() + ":" +
+                            entry.getValue().toString()+"\n"+ System.getProperty("line.separator");
+                    ;
+                }
+
+                textViewScore.setText(texto);
+
+            }
+
+        }else{
+            Toast.makeText(this, "ERRO no arquivo", Toast.LENGTH_SHORT).show();
+        }//if
+
+
+
+
+    }//lerTudo
     public void cliqueVoltar(View view){
         chamaMenuActivity();
     }//cliqueVoltar
@@ -38,4 +81,5 @@ public class RankingActivity extends AppCompatActivity {
         finishAffinity(); //Método para matar a activity e não deixa-lá indexada na pilhagem
         return;
     }
+
 }
